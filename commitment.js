6124167562a00658c3f0857843ccb9686efdad5d
Teddy Robinson <thro1264@colorado.edu>
2020-02-24 14:06:44 -0700

    Adding images

--- commitment.js
+++ commitment.js
@@ -8,13 +8,13 @@ var slideIndex = 0;
 slideshow();
 
 function slideshow() {
-  var i;
-  var x = document.getElementsByClassName("slide");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(slideshow, 2000);
+	var i;
+	var x = document.getElementsByClassName("slide");
+	for (i = 0; i < x.length; i++) {
+	x[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > x.length) {slideIndex = 1}
+	x[slideIndex-1].style.display = "block";
+	setTimeout(slideshow, 2000);
 }
