275ebc6b63f1139de0db67d01824e1f168778a11
Teddy Robinson <thro1264@colorado.edu>
2020-02-24 13:54:42 -0700

    Reverting to normal

---
+++ commitment.js
@@ -0,0 +1,4 @@
+// document.getElementById('hover-info').style.display="hidden";
+// document.getElementbyId('hover-info').onclick=function{
+// 	document.getElementById('hover-info').style.display="visible";
+// }
