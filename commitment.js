54f9bb5729a708aa0e3f0fc13239658151aa158f
Teddy Robinson <thro1264@colorado.edu>
2020-02-24 14:26:29 -0700

    Testing slideshow JS

--- commitment.js
+++ commitment.js
@@ -5,9 +5,9 @@
 // Code found on W3 Schools https://www.w3schools.com/w3css/w3css_slideshow.asp
 
 var slideIndex = 0;
-slideshow();
+_slideshow();
 
-function slideshow() {
+function _slideshow() {
 	var i;
 	var x = document.getElementsByClassName("slide");
 	for (i = 0; i < x.length; i++) {
@@ -16,5 +16,5 @@ function slideshow() {
 	slideIndex++;
 	if (slideIndex > x.length) {slideIndex = 1}
 	x[slideIndex-1].style.display = "block";
-	setTimeout(slideshow, 2000);
+	setTimeout(_slideshow, 2000);
 }
